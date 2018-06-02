19ee7032480ae6e56cf0c7221d6d5252d9b64d24
Muragijimana <beastar457@gmail.com>
2018-06-02 16:57:13 +0200

    tour

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,18 +8,63 @@
     <title>Provisoire</title>
 
     <link rel="stylesheet" href="{{asset ('css/app.css')}}">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <!-- Meterial Icon CSS -->
+    <link rel="stylesheet" href="{{asset('css/materialdesignicons.min.css')}}">
+    <!-- Material CSS -->
+    <link rel="stylesheet" href="{{asset('css/material.min.css')}}">
+    <!-- Ripples CSS -->
+    <link rel="stylesheet" href="{{asset('css/ripples.min.css')}}">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+    <!-- Slicknav CSS -->
+    <link rel="stylesheet" href="{{asset('css/slicknav.css')}}">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+
+
+    <!-- Color CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/colors/indigo.css')}}" media="screen"/>
+
 </head>
 <body>
 
-    <div id="app">
-        <main class="container">
-            @yield('content')
-        </main>
-    </div>
+<div id="app">
+    <main class="container">
+        @yield('content')
+    </main>
+</div>
 </body>
 
 <script src="{{asset ('js/app.js')}}"></script>
-
+<script src="{{asset('js/jquery-min.js')}}"></script>
+<script src="{{asset('js/popper.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/jquery.mixitup.min.js')}}"></script>
+<script src="{{asset('js/jquery.inview.js')}}"></script>
+<script src="{{asset('js/jquery.counterup.min.js')}}"></script>
+<script src="{{asset('js/scroll-top.js')}}"></script>
+<script src="{{asset('js/smoothscroll.js')}}"></script>
+<script src="{{asset('js/material.min.js')}}"></script>
+<script src="{{asset('js/ripples.min.js')}}"></script>
+<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('js/form-validator.min.js')}}"></script>
+<script src="{{asset('js/contact-form-script.min.js')}}"></script>
+<script src="{{asset('js/wow.js')}}"></script>
+<script src="{{asset('js/jquery.vide.js')}}"></script>
+<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('js/jquery.slicknav.js')}}"></script>
+<script src="{{asset('js/main.js')}}"></script>
 
 <script type="text/javascript">
     $(document).ready(function () {
