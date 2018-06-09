e208ea216135441270c77ea44f8d44d3caadcd17
Muragijimana <beastar457@gmail.com>
2018-06-09 21:34:26 +0200

    w

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::post('add_account', 'AccountController@index')->name('home');
 Route::get('get_user', 'User@getUser')->name('home');
 Route::get('get_accounts', 'AccountController@getAccount')->name('home');
-Route::get('get_data', 'DataController@getData')->name('data');
-
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('questions', 'HomeController@questions');
-Route::get('question', 'HomeController@getAnswers');
-//Route::get('question/{}', 'HomeController@getAnswers');
-Route::post('add_question', 'HomeController@addQuestion');
-Route::get('add_answer/{id}', 'HomeController@addAnswer');
-
-Route::get('save_answer/{id}/{a_id}', 'HomeController@saveAnswer');
-Route::get('save_answers', 'HomeController@saveAnswerApi');
-Route::get('get_marks', 'HomeController@getMarks');
 Route::post('upload', 'HomeController@upload');
 
